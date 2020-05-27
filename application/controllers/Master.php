a63ff4758b64607268ad9402fa6e087ec17abce4
desal489 <desalisra48@gmail.com>
2020-05-28 06:59:41 +0700

    menambahkan fitur import excel datasiswa

--- application/controllers/Master.php
+++ application/controllers/Master.php
@@ -1,7 +1,13 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Master extends CI_Controller {
+class Master extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('master/Master_model');
+	}
 
 	public function index()
 	{
@@ -11,7 +17,7 @@ class Master extends CI_Controller {
 		$this->load->view('templates/footer');
 	}
 
-	
+
 	// Data Tahunan
 	public function dataTahunan()
 	{
@@ -20,8 +26,6 @@ class Master extends CI_Controller {
 		$this->load->view('master/dataTahunan_view');
 		$this->load->view('templates/footer');
 	}
-
-
 
 
 	// Siswa
@@ -33,14 +37,42 @@ class Master extends CI_Controller {
 		$this->load->view('templates/footer');
 	}
 
+	public function importSiswa()
+	{
+		// Import Library
+		include APPPATH . 'third_party/excel_reader2/excel_reader2.php';
 
+		// upload file xls
+		$target = basename($_FILES['fileSiswa']['name']);
+		move_uploaded_file($_FILES['fileSiswa']['tmp_name'], $target);
 
+		// beri permision  agar file xls dapat di baca
+		chmod($_FILES['fileSiswa']['name'], 0777);
 
+		// mengambil isi file xls
+		$data = new Spreadsheet_Excel_Reader($_FILES['fileSiswa']['name'], false);
+		// menghitung jumlah baris data yang ada
+		$jumlah_baris = $data->rowcount($sheet_index = 0);
 
+		// jumlah default data yang berhasil di import
+		$berhasil = 0;
+		for ($i = 2; $i <= $jumlah_baris; $i++) {
 
+			// menangkap data dan memasukkan ke variabel sesuai dengan kolumnya masing-masing
+			$nis     = $data->val($i, 1);
+			$nama     = $data->val($i, 2);
+			$kelas   = $data->val($i, 3);
+			$wali  = $data->val($i, 4);
+			$alamat  = $data->val($i, 4);
 
+			if ($nis != "" && $nama != "" && $kelas != "" && $wali != "" && $alamat != "") {
+				// input data ke database (table data_pegawai)
+				$this->Mpesan_model->getData($nis, $nama, $kelas, $wali, $alamat);
+				$berhasil++;
+			}
+		}
 
-	
-
-
+		// hapus kembali file .xls yang di upload tadi
+		unlink($_FILES['filepegawai']['name']);
+	}
 }
