330f781b6048c8abb0e88acc1518b8fea72fe554
unknown <Genta>
2020-05-28 02:59:38 +0700

    menambah fitur crud pada kelas dan tahun ajaran

--- application/controllers/Master.php
+++ application/controllers/Master.php
@@ -1,7 +1,13 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Master extends CI_Controller {
+class Master extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Master_model', 'master');
+	}
 
 	public function index()
 	{
@@ -11,18 +17,18 @@ class Master extends CI_Controller {
 		$this->load->view('templates/footer');
 	}
 
-	
+
 	// Data Tahunan
 	public function dataTahunan()
 	{
+
+		$data['tahun_ajar'] = $this->db->get('tahun_ajaran')->result_array();
+		$data['kelas'] = $this->master->getKelas();
 		$this->load->view('templates/header');
 		$this->load->view('templates/menu');
-		$this->load->view('master/dataTahunan_view');
+		$this->load->view('master/dataTahunan_view', $data);
 		$this->load->view('templates/footer');
 	}
-
-
-
 
 	// Siswa
 	public function siswa()
@@ -32,15 +38,60 @@ class Master extends CI_Controller {
 		$this->load->view('master/siswa_view');
 		$this->load->view('templates/footer');
 	}
+	public function tambahTahunAjaran()
+	{
+		$data = htmlspecialchars(filter_var($this->input->post('tahun_ajaran')));
+		$this->master->insertTahunAjaran($data);
+	}
 
+	public function editTahunAjar()
+	{
+		if (empty($_POST)) {
+			redirect('master');
+		}
+		$thnAjar = $this->input->post('edit_tahunAjar');
+		$id = $this->input->post('ta_id');
+		$this->master->editTahunAjaran($thnAjar, $id);
+	}
 
+	public function hapusTahunAjar($id)
+	{
+		$this->master->deleteTahunAjaran($id);
+	}
 
+	public function getkelas()
+	{
+		$id = htmlspecialchars(filter_var($_POST['id'], FILTER_SANITIZE_STRING));
+		echo json_encode($this->master->getKelasId($id));
+	}
 
+	public function tambahKelas()
+	{
+		if (empty($_POST)) {
+			redirect('master');
+		}
 
+		$idTahunAjaran = $this->input->post('id_tahun_ajaran');
+		$tingkatan = $this->input->post('tingkatan');
+		$nama_kelas = $this->input->post('nama_kelas');
 
+		$this->master->insertKelas($idTahunAjaran, $tingkatan, $nama_kelas);
+	}
 
+	public function editKelas()
+	{
+		if (empty($_POST)) {
+			redirect('master');
+		}
+		$id = $this->input->post('kelas_id');
+		$idTahunAjaran = $this->input->post('id_tahun_ajaran');
+		$nama_kelas = $this->input->post('nama_kelas');
+		$tingkat = $this->input->post('tingkatan');
+		$this->master->editkelas($id, $idTahunAjaran, $nama_kelas, $tingkat);
+	}
 
-	
-
-
+	public function hapusKelas($id)
+	{
+		$this->master->deleteKelas($id);
+	}
 }
